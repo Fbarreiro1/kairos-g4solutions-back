c25a90b730d585da10b30297ca2bd7255987afff
unknown <lgiallonardi@gmail.com>
2024-06-22 22:47:35 -0300

    commit produccion

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
     DB_USER: process.env.DB_USER || 'tp_g4',
     DB_PASSWORD: process.env.DB_PASSWORD || '123456789',
     DB_NAME: process.env.DB_NAME || 'tp_g4_db',
+    DB_PORT: process.env.DB_PORT,
     PORT: process.env.PORT || 3000
   };
