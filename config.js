4790b92feb5c016c998db20c750c8a5e53e68ccb
unknown <lgiallonardi@gmail.com>
2024-06-23 14:35:05 -0300

    commit produccion

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    DB_HOST : process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'tp_g4',
-    DB_PASSWORD: process.env.DB_PASSWORD || '123456789',
-    DB_NAME: process.env.DB_NAME || 'tp_g4_db',
-    DB_PORT: process.env.DB_PORT,
+    DB_HOST : process.env.DB_HOST || 'roundhouse.proxy.rlwy.net',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'SRDyseIrbvhhAovmfFRQcFIhOLSSAkUm',
+    DB_NAME: process.env.DB_NAME || 'railway',
+    DB_PORT: process.env.DB_PORT || 27052,
     PORT: process.env.PORT || 3000
   };
